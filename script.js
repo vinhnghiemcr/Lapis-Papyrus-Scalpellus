226237c1625b025c6eac5e03e40781faf39198d0
Nghiem Truong <vinhnghiemcr@gmail.com>
2022-01-24 03:13:43 -0600

    Update the logic of the game

--- script.js
+++ script.js
@@ -60,25 +60,25 @@ if (userChoice == gameOptions[0]){
   if (userChoice === computerChoice){
     display("We have a tie! The player and the computer choose " + gameOptions[0]);    
   } else if (computerChoice === gameOptions[1]) {
-          display("The user wins! The user chooses " + gameOptions[0] + " and the computer chooses " + gameOptions[1]);   
+          display("The computer wins! The player chooses " + gameOptions[0] + " and the computer chooses " + gameOptions[1]);   
          } else  {
-                  display("The computer wins! The user chooses " + gameOptions[0] + " and the computer chooses " + gameOptions[2]); 
+                  display("The player wins! The player chooses " + gameOptions[0] + " and the computer chooses " + gameOptions[2]); 
                 }
 } else if (userChoice === gameOptions[1]){
           if (computerChoice === gameOptions[0]){
-            display("The computer wins! The user chooses " + gameOptions[1] + " and the computer chooses " + gameOptions[0]);
+            display("The player wins! The player chooses " + gameOptions[1] + " and the computer chooses " + gameOptions[0]);
           } else if (computerChoice === gameOptions[1]){
             display("We have a tie! The user chooses and the computer chooses " + gameOptions[1]);
           } else {
-            display("The user wins! The user chooses " + gameOptions[1] + " and the computer chooses " + gameOptions[2]);
+            display("The computer wins! The player chooses " + gameOptions[1] + " and the computer chooses " + gameOptions[2]);
           }
 } else {
   if (computerChoice === gameOptions[0]){
-            display("The user wins! The user chooses " + gameOptions[2] + " and the computer chooses " + gameOptions[0]);
+            display("The computer wins! The player chooses " + gameOptions[2] + " and the computer chooses " + gameOptions[0]);
           } else if (computerChoice === gameOptions[1]){
-            display("The computer wins! The user chooses " + gameOptions[2] + " and the computer chooses " + gameOptions[1]);
+            display("The player wins! The player chooses " + gameOptions[2] + " and the computer chooses " + gameOptions[1]);
           } else {
-            display("We have a tie! The user and the computer choos " + gameOptions[2]);
+            display("We have a tie! The player and the computer choose " + gameOptions[2]);
           }
 };
 }
