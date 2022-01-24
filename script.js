2b3e47291446af4b80f11b77bc7c12681ca0592c
Nghiem Truong <vinhnghiemcr@gmail.com>
2022-01-23 19:31:29 -0600

    Create user and computer objects along with their option to play

---
+++ script.js
@@ -0,0 +1,11 @@
+//gameOptions array will hold 3 values that a player can posibblely choose from
+let gameOptions = ["Lapis", "Papyrus", "Scalpellus"]
+
+//Create a user object
+let user = {
+  choice: null
+}
+//create computer object
+let computer = {
+  choice: null
+}
