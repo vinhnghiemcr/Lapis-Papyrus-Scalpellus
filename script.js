0aba30f3264cba947ffbec4209574ea4d2219ca6
Nghiem Truong <vinhnghiemcr@gmail.com>
2022-01-23 19:33:52 -0600

    Create the logic of the game

--- script.js
+++ script.js
@@ -13,3 +13,32 @@ let computer = {
 const computerChooses = () => {
     return gameOptions[Math.floor(Math.random()*gameOptions.length)];
   }
+
+//Function compareChoices()  determine who wins
+const compareChoices = (userChoice, computerChoice) => {
+    if (userChoice == gameOptions[0]){
+      if (userChoice === computerChoice){
+        console.log("We have a tie! The player and the computer choose " + gameOptions[0]);    
+      } else if (computerChoice === gameOptions[1]) {
+              console.log("The user wins! The user chooses " + gameOptions[0] + " and the computer chooses " + gameOptions[1]);   
+             } else  {
+                      console.log("The computer wins! The user chooses " + gameOptions[0] + " and the computer chooses " + gameOptions[2]); 
+                    }
+    } else if (userChoice === gameOptions[1]){
+              if (computerChoice === gameOptions[0]){
+                console.log("The computer wins! The user chooses " + gameOptions[1] + " and the computer chooses " + gameOptions[0]);
+              } else if (computerChoice === gameOptions[1]){
+                console.log("We have a tie! The user chooses and the computer chooses " + gameOptions[1]);
+              } else {
+                console.log("The user wins! The user chooses " + gameOptions[1] + " and the computer chooses " + gameOptions[2]);
+              }
+    } else {
+      if (computerChoice === gameOptions[0]){
+                console.log("The user wins! The user chooses " + gameOptions[2] + " and the computer chooses " + gameOptions[0]);
+              } else if (computerChoice === gameOptions[1]){
+                console.log("The computer wins! The user chooses " + gameOptions[2] + " and the computer chooses " + gameOptions[1]);
+              } else {
+                console.log("We have a tie! The user and the computer choos " + gameOptions[2]);
+              }
+    };
+    }
